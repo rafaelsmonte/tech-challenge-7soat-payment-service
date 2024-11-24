9d7e5989a51a16f0df28c7de536eee0b7b6f4643
Rafael Santos Monte <rafael.monte@intelbras.com.br>
2024-11-24 17:49:31 +0000

    payment tests

--- src/external/prisma-database.external.spec.ts
+++ src/external/prisma-database.external.spec.ts
@@ -1,170 +1,202 @@
-import { Prisma, PrismaClient, Payment as PrismaPayment} from '@prisma/client';
 import { PrismaDatabase } from './prisma-database.external';
 import { Payment } from '../entities/payment.entity';
-import { DatabaseError } from '../errors/database.error';
+import { PaymentStatus } from '../enum/payment-status.enum';
+import Decimal from 'decimal.js';
+import { Prisma } from '@prisma/client';
 
+jest.mock('@prisma/client', () => {
+  class MockDecimal {
+    private value: number;
 
-const mockPayments: PrismaPayment[] = [
-    {
-      id: 1,
-      externalId: BigInt(111),
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      orderId: 12,
-      status: 'PENDING',
-      price: new Prisma.Decimal(100),
-      pixQrCode: 'qr1',
-      pixQrCodeBase64: 'base64qr1',
+    constructor(value: number) {
+      this.value = value;
+    }
+
+    toNumber() {
+      return this.value;
+    }
+
+    plus(amount: number) {
+      return new MockDecimal(this.value + amount);
+    }
+  }
+  return {
+    Decimal: MockDecimal,
+    PrismaClient: jest.fn(() => prismaMock),
+    PaymentStatus: {
+      PENDING: 'PENDING',
+      SUCCESS: 'SUCCESS',
+      FAILED: 'FAILED',
     },
-  ];
-  
-  const mockCreatedPayment: PrismaPayment = {
-    id: 1,
-    externalId: BigInt(111),
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    orderId: 12,
-    status: 'PENDING',
-    price: new Prisma.Decimal(100),
-    pixQrCode: 'qr1',
-    pixQrCodeBase64: 'base64qr1',
   };
-  
-  const mockUpdatedPayment: PrismaPayment = {
-    id: 1,
-    externalId: BigInt(111),
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    orderId: 12,
-    status: 'SUCCESS', // Updated status
-    price: new Prisma.Decimal(100),
-    pixQrCode: 'qr1',
-    pixQrCodeBase64: 'base64qr1',
-  };
-  
-  // Mock Implementation
-  jest.mock('@prisma/client', () => {
-    return {
-      PrismaClient: jest.fn().mockImplementation(() => ({
-        payment: {
-          findMany: jest.fn(),
-          findUnique: jest.fn(),
-          create: jest.fn(),
-          update: jest.fn(),
-        },
-      })),
-      Prisma: {
-        PaymentStatus: {
-          PENDING: 'PENDING',
-          SUCCESS: 'SUCCESS',
-        },
-        Decimal: jest.fn().mockImplementation((value: number) => ({
-          toString: () => value.toString(),
-          toNumber: () => value,
-        })),
-      },
-    };
-  });
-  
-  describe('PrismaDatabase', () => {
-    let prismaDatabase: PrismaDatabase;
-    let prismaClientMock: jest.Mocked<PrismaClient>;
-  
-    beforeEach(() => {
-      prismaClientMock = new PrismaClient() as jest.Mocked<PrismaClient>;
-      prismaDatabase = new PrismaDatabase();
-    });
-  
-    // describe('findAllPayments', () => {
-    //   it('should return all payments', async () => {
-    //     (prismaClientMock.payment.findMany as jest.Mock).mockResolvedValue(mockPayments);
-  
-    //     const result = await prismaDatabase.findAllPayments();
+});
 
-  
-    //     expect(result).toHaveLength(1);
-    //     expect(result[0]).toBeInstanceOf(Payment);  // Ensure Payment entity
-    //     expect(result[0].getPrice()).toBe(100);  // Assuming Payment has this method
-    //   });
-  
-    //   it('should throw a DatabaseError when it fails', async () => {
-    //     (prismaClientMock.payment.findMany as jest.Mock).mockRejectedValue(new Error('DB error'));
-  
-    //     await expect(prismaDatabase.findAllPayments()).rejects.toThrow(DatabaseError);
-    //   });
-    // });
-  
-    // describe('findPaymentById', () => {
-    //   it('should return a payment by id', async () => {
-    //     (prismaClientMock.payment.findUnique as jest.Mock).mockResolvedValue(mockPayments[0]);
-  
-    //     const result = await prismaDatabase.findPaymentById(1);
-  
-    //     expect(result).toBeInstanceOf(Payment);
-    //     expect(result?.getPrice()).toBe(100);
-    //   });
-  
-    //   it('should return null if payment not found', async () => {
-    //     (prismaClientMock.payment.findUnique as jest.Mock).mockResolvedValue(null);
-  
-    //     const result = await prismaDatabase.findPaymentById(1);
-  
-    //     expect(result).toBeNull();
-    //   });
-  
-    //   it('should throw a DatabaseError when it fails', async () => {
-    //     (prismaClientMock.payment.findUnique as jest.Mock).mockRejectedValue(new Error('DB error'));
-  
-    //     await expect(prismaDatabase.findPaymentById(1)).rejects.toThrow(DatabaseError);
-    //   });
-    // });
-  
-    // describe('createPayment', () => {
-    //   it('should create and return a payment', async () => {
-    //     const paymentToCreate = new Payment(0, 123, new Date(), new Date(), 12, 'PENDING', 100, 'qr1', 'base64qr1');
-    //     (prismaClientMock.payment.create as jest.Mock).mockResolvedValue(mockCreatedPayment);
-  
-    //     const result = await prismaDatabase.createPayment(paymentToCreate);
-  
-    //     expect(result).toBeInstanceOf(Payment);
-    //     expect(result.getPrice()).toBe(100);
-    //     expect(prismaClientMock.payment.create).toHaveBeenCalled();
-    //   });
-  
-    //   it('should throw a DatabaseError when it fails', async () => {
-    //     const paymentToCreate = new Payment(0, 123, new Date(), new Date(), 12, 'PENDING', 100, 'qr1', 'base64qr1');
-    //     (prismaClientMock.payment.create as jest.Mock).mockRejectedValue(new Error('DB error'));
-  
-    //     await expect(prismaDatabase.createPayment(paymentToCreate)).rejects.toThrow(DatabaseError);
-    //   });
-    // });
-  
-    describe('updatePaymentStatus', () => {
-    //   it('should update and return the updated payment', async () => {
-    //     const paymentToUpdate = new Payment(1, 123, new Date(), new Date(), 12, 'PENDING', 100, 'qr1', 'base64qr1');
-    //     (prismaClientMock.payment.update as jest.Mock).mockResolvedValue(mockUpdatedPayment);
-  
-    //     const result = await prismaDatabase.updatePaymentStatus(paymentToUpdate);
-  
-    //     expect(result).toBeInstanceOf(Payment);
-    //     expect(result.getStatus()).toBe('PAID'); // Ensure proper status update
-    //   });
-  
-    //   it('should return null if payment is not found', async () => {
-    //     const paymentToUpdate = new Payment(1, 123, new Date(), new Date(), 12, 'PENDING', 100, 'qr1', 'base64qr1');
-    //     (prismaClientMock.payment.update as jest.Mock).mockResolvedValue(null);
-  
-    //     const result = await prismaDatabase.updatePaymentStatus(paymentToUpdate);
-  
-    //     expect(result).toBeNull();
-    //   });
-  
-      it('should throw a DatabaseError when it fails', async () => {
-        const paymentToUpdate = new Payment(1, 123, new Date(), new Date(), 12, 'PENDING', 100, 'qr1', 'base64qr1');
-        (prismaClientMock.payment.update as jest.Mock).mockRejectedValue(new Error('DB error'));
-  
-        await expect(prismaDatabase.updatePaymentStatus(paymentToUpdate)).rejects.toThrow(DatabaseError);
-      });
+const prismaMock = {
+  payment: {
+    findMany: jest.fn(),
+    findUnique: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+  },
+};
+
+describe('PrismaDatabase', () => {
+  let database: PrismaDatabase;
+
+  beforeEach(() => {
+    database = new PrismaDatabase();
+  });
+
+  const mockPayment = new Payment(
+    1,
+    101,
+    new Date('2024-01-01T00:00:00Z'),
+    new Date('2024-01-02T00:00:00Z'),
+    123,
+    PaymentStatus.PENDING,
+    200.75,
+    'someQrCode',
+    'someBase64',
+  );
+
+  describe('findAllPayments', () => {
+    it('should return a list of payments', async () => {
+      const mockPayments = [
+        {
+          id: 1,
+          externalId: '101',
+          createdAt: new Date('2024-01-01T00:00:00Z'),
+          updatedAt: new Date('2024-01-02T00:00:00Z'),
+          orderId: 123,
+          status: PaymentStatus.PENDING,
+          price: new Decimal(200.75).toNumber(),
+          pixQrCode: 'someQrCode',
+          pixQrCodeBase64: 'someBase64',
+        },
+      ];
+
+      (prismaMock.payment.findMany as jest.Mock).mockResolvedValue(mockPayments);
+
+      const payments = await database.findAllPayments();
+
+      expect(payments).toHaveLength(1);
+      expect(payments[0]).toBeInstanceOf(Payment);
+      expect(payments[0].getId()).toBe(1);
+    });
+
+    it('should throw a DatabaseError if findMany fails', async () => {
+      (prismaMock.payment.findMany as jest.Mock).mockRejectedValue(
+        new Error('Database error'),
+      );
+
+      await expect(database.findAllPayments()).rejects.toThrowError(
+        'Failed to find payments',
+      );
     });
   });
-  
+
+  describe('findPaymentById', () => {
+    it('should return a payment by ID', async () => {
+      (prismaMock.payment.findUnique as jest.Mock).mockResolvedValue({
+        id: 1,
+        externalId: '101',
+        createdAt: new Date('2024-01-01T00:00:00Z'),
+        updatedAt: new Date('2024-01-02T00:00:00Z'),
+        orderId: 123,
+        status: PaymentStatus.PENDING,
+        price: new Decimal(200.75).toNumber(),
+        pixQrCode: 'someQrCode',
+        pixQrCodeBase64: 'someBase64',
+      });
+
+      const payment = await database.findPaymentById(1);
+
+      expect(payment).toBeInstanceOf(Payment);
+      expect(payment?.getId()).toBe(1);
+    });
+
+    it('should return null if payment is not found', async () => {
+      (prismaMock.payment.findUnique as jest.Mock).mockResolvedValue(null);
+
+      const payment = await database.findPaymentById(999);
+
+      expect(payment).toBeNull();
+    });
+
+    it('should throw a DatabaseError if findUnique fails', async () => {
+      (prismaMock.payment.findUnique as jest.Mock).mockRejectedValue(
+        new Error('Database error'),
+      );
+
+      await expect(database.findPaymentById(1)).rejects.toThrowError(
+        'Failed to find payments',
+      );
+    });
+  });
+
+  // describe('createPayment', () => {
+  //   it('should create a payment', async () => {
+  //     const price = new Prisma.Decimal(200.50);
+  //     const mockPrismaPayment = {
+  //       id: 1,
+  //       externalId: '101',
+  //       createdAt: new Date('2024-01-01T00:00:00Z'),
+  //       updatedAt: new Date('2024-01-02T00:00:00Z'),
+  //       orderId: 123,
+  //       status: new Decimal(200),//PaymentStatus.PENDING,
+  //       price: price,
+  //       pixQrCode: 'someQrCode',
+  //       pixQrCodeBase64: 'someBase64',
+  //     };
+
+  //     (prismaMock.payment.create as jest.Mock).mockResolvedValue(
+  //       mockPrismaPayment,
+  //     );
+
+  //     const payment = await database.createPayment(mockPayment);
+
+  //     expect(payment).toBeInstanceOf(Payment);
+  //     expect(payment.getId()).toBe(1);
+  //   });
+
+  //   it('should throw a DatabaseError if create fails', async () => {
+  //     (prismaMock.payment.create as jest.Mock).mockRejectedValue(
+  //       new Error('Database error'),
+  //     );
+
+  //     await expect(database.createPayment(mockPayment)).rejects.toThrowError(
+  //       'Failed to save payment',
+  //     );
+  //   });
+  // });
+
+  describe('updatePaymentStatus', () => {
+    it('should update the status of a payment', async () => {
+      const mockUpdatedPayment = {
+        ...mockPayment,
+        status: PaymentStatus.SUCCESS,
+      };
+
+      (prismaMock.payment.update as jest.Mock).mockResolvedValue(
+        mockUpdatedPayment,
+      );
+
+      const updatedPayment = await database.updatePaymentStatus(
+        mockPayment,
+      );
+
+      expect(updatedPayment).toBeInstanceOf(Payment);
+      expect(updatedPayment.getStatus()).toBe(PaymentStatus.SUCCESS);
+    });
+
+    it('should throw a DatabaseError if update fails', async () => {
+      (prismaMock.payment.update as jest.Mock).mockRejectedValue(
+        new Error('Database error'),
+      );
+
+      await expect(
+        database.updatePaymentStatus(mockPayment),
+      ).rejects.toThrowError('Failed to update payment status');
+    });
+  });
+});
